1144b58be31abf569e2f00ea9a6304a745b3d97b
DESKTOP-M6UQO63\witta <wittawatrk@gmail.com>
2019-05-09 10:21:45 +0700

    first initial

---
+++ PDF/PDF.php
@@ -0,0 +1,46 @@
+<?php
+
+
+class PDF extends TCPDF
+{
+    public $FONT_NAME = "thsarabun";
+    public $FONT_SIZE = 13;
+    public $CELL_H = 5;
+
+    function __construct($orientation = "P", $unit = "mm", $format = "A4", $unicode = true, $encoding = "UTF8", $diskcache = false, $pdfa = false)
+    {
+        parent::__construct($orientation, $unit, $format, $unicode, $encoding, $diskcache, $pdfa);
+
+        // remove default header/footer
+//        $this->setPrintHeader(false);
+//        $this->setPrintFooter(false);
+
+        // add font TH Sarabun
+        $this->AddFont($this->FONT_NAME, "", "thsarabun.php");
+        $this->AddFont($this->FONT_NAME, "b", "thsarabunb.php");
+        $this->AddFont($this->FONT_NAME, "i", "thsarabuni.php");
+        $this->AddFont($this->FONT_NAME, "bi", "thsarabunbi.php");
+
+        // set font
+        $this->setCellHeightRatio(1);
+        $this->SetFont($this->FONT_NAME, '', $this->FONT_SIZE);
+        $this->setHeaderFont(array($this->FONT_NAME, '', $this->FONT_SIZE));
+        $this->setFooterFont(array($this->FONT_NAME, '', $this->FONT_SIZE));
+
+        // set margin
+        $this->lMargin = 5;
+        $this->rMargin = 5;
+        $this->tMargin = 23;
+    }
+
+    public function getLMargin()
+    {
+        return $this->lMargin;
+    }
+    public function Footer(){
+        $this->setY(-7);
+        $this->SetFont('helvetica', 'I', 6);
+        $this->Cell(0,8,'www.shippop.com',0,0);
+        $this->Image('logo.png', 69, 76, 8.73, 1.6, 'PNG', '', '', true, 150, '', false, false, 0, false, false, false);
+    }
+}
---
+++ test2.php
@@ -0,0 +1,74 @@
+<?php
+include("TCPDF/tcpdf.php");
+include("PDF/pdf.php");
+$pageSize = array(80, 80);
+$pdf = new PDF(PDF_PAGE_ORIENTATION, PDF_UNIT, $pageSize, true, 'UTF-8', false);
+// set document information
+$pdf->SetCreator(PDF_CREATOR);
+$pdf->SetAuthor('wittawat');
+$pdf->SetTitle('Sticker Example 8x8');
+$pdf->SetSubject('Sticker');
+$pdf->setPrintHeader(false);
+$pdf->setPrintFooter(true);
+$pdf->SetAutoPageBreak(FALSE, 0);
+$style = array(
+	'position' => '',
+	'align' => 'C',
+	'stretch' => true,
+	'fitwidth' => true,
+	'cellfitalign' => '',
+	'border' => false,
+	'hpadding' => 'auto',
+	'vpadding' => 'auto',
+	'fgcolor' => array(0, 0, 0),
+	'bgcolor' => false, //array(255,255,255),
+	'text' => true,
+	'font' => 'helvetica',
+	'fontsize' => 8,
+	'stretchtext' => 4
+);
+// set default monospaced font
+// $pdf->SetDefaultMonospacedFont(PDF_FONT_MONOSPACED);
+// set margins
+ $pdf->SetMargins(2, 2, 2);
+
+// set auto page breaks
+// $pdf->SetAutoPageBreak(TRUE,0);
+$pdf->AddPage();
+
+$pdf->Image('logo.png', 5, 4, 31.75, 5.749, 'PNG', '', '', true, 150, '', false, false, 0, false, false, false);
+$pdf->Image('flash_express.png', 41.75, 2, 31.75, 9.617604167, 'PNG', '', '', true, 150, '', false, false, 0, false, false, false);
+
+$pdf->write1DBarcode('SP050287118', 'C128', '8', '11', '', 18, 0.4, $style, 'C');
+$html = <<<EOD
+<table cellspacing="0" cellpadding="2">
+    <tr>
+        <td colspan="4" style = "border: 1px solid black;font-size:12px"><b>ผู้ส่ง</b> ตาล จิรกานต์( โทร. 081544470 )
+            <br> ตาล จิรกานต์ 178/109 ถนน ประชาสโมสร ตำบลในเมือง อำเภอเมือง ขอนแก่น 40000 40000
+        </td>
+    </tr>
+    <tr>
+        <td colspan="4" style = "border-left: 1px solid black;border-right: 1px solid black;font-size:16px ">(ผู้รับ) Nize Seasonings (0991565055)
+            <br> 16/53 ถนนบางขุนเทียน-ชายทะเล ซ.เทียนทะเล 20 แสมดำ บางขุนเทียน กทม</td>
+    </tr>
+    <tr>
+        <td colspan="3" style = "border-left: 1px solid black;width:82%"></td>
+        <td style="width:18%;font-size:18px;border: 1px solid black;text-align:right;border-top:1px solid black">10150</td>
+    </tr>
+    <tr >
+        <td style = "border: 1px solid black;" colspan="2"></td>
+        <td style = "border: 1px solid black; font-size:17px;text-align:center" colspan="2"><b>ไม่เก็บเงินปลายทาง</b></td>
+    </tr>
+
+</table>
+EOD;
+$pdf->SetY(28);
+$pdf->writeHTML($html, true, false, false, false, '');
+
+
+
+
+
+$pdf->Output('example_001.pdf', 'I');
+//============================================================+
+// END OF FILE
